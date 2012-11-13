60774788645c06f5a940113dc57af5cc40393b32
Christopher Best <bestchris@gmail.com>
2012-11-13 12:07:33 -0500

    write mouse click locations to file; ignore output folder

--- bin/ExpDataServer.dart
+++ bin/ExpDataServer.dart
@@ -1,19 +1,30 @@
 import "dart:io";
 
 void main() {
+  
   HttpServer server = new HttpServer();
   WebSocketHandler wsHandler = new WebSocketHandler();
+  File dataFile = new File.fromPath(new Path("output/data.txt"));
+  OutputStream stream;
   server.addRequestHandler((req) => req.path == "/ws", wsHandler.onRequest);
   
   wsHandler.onOpen = (WebSocketConnection conn) {
+    // open file stream
+    stream = dataFile.openOutputStream();
+    
     print('new connection');
     
     conn.onMessage = (message) {
       print("mouse click at: $message");
+      // write mouse click location to file
+      stream.writeString("$message\n");
     };
     
     conn.onClosed = (int status, String reason) {
       print('closed with $status for $reason');
+      
+      // close stream
+      stream.close();
     };
   };
   
