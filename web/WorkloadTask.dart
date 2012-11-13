a828c3883b5f077be5242d8af28e4b6d89c24ef3
Christopher Best <bestchris@gmail.com>
2012-11-13 14:15:01 -0500

    create and show a target; notify server on target mouse down

--- web/WorkloadTask.dart
+++ web/WorkloadTask.dart
@@ -12,4 +12,13 @@ void main() {
     // notify data server of click
     ws.send("${event.screenX}, ${event.screenY}");
   });
+  
+  // create target display
+  Element target = new DivElement()..classes.add("target")
+      ..style.left = "200px"
+      ..style.top = "200px";
+  document.body.elements.add(target);
+  target.on.mouseDown.add((MouseEvent e) {
+    ws.send("target hit: ${e.clientX}, ${e.clientY}");
+  });
 }
