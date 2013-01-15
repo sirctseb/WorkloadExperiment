7e13b3a29047298ae22f613b07b20835bb430794
Christopher Best <bestchris@gmail.com>
2013-01-15 14:46:57 -0500

    put server work into a class

--- bin/ExpDataServer.dart
+++ bin/ExpDataServer.dart
@@ -1,32 +1,40 @@
 import "dart:io";
 
 void main() {
-  
+  Server server = new Server();
+}
+
+class Server {
   HttpServer server = new HttpServer();
   WebSocketHandler wsHandler = new WebSocketHandler();
   File dataFile = new File.fromPath(new Path("output/data.txt"));
   OutputStream stream;
-  server.addRequestHandler((req) => req.path == "/ws", wsHandler.onRequest);
-  
-  wsHandler.onOpen = (WebSocketConnection conn) {
-    // open file stream
-    stream = dataFile.openOutputStream();
+  Server() {
+    server.addRequestHandler((req) => req.path == "/ws", wsHandler.onRequest);
     
-    print('new connection');
-    
-    conn.onMessage = (message) {
-      //print("mouse click at: $message");
-      // write mouse click location to file
-      stream.writeString("$message\n");
-    };
-    
-    conn.onClosed = (int status, String reason) {
-      print('closed with $status for $reason');
+    wsHandler.onOpen = (WebSocketConnection conn) {
       
-      // close stream
-      stream.close();
+      // open file stream
+      stream = dataFile.openOutputStream();
+      
+      print(dataFile.fullPathSync());
+      
+      print('new connection');
+      
+      conn.onMessage = (message) {
+        //print("mouse click at: $message");
+        // write mouse click location to file
+        stream.writeString("$message\n");
+      };
+      
+      conn.onClosed = (int status, String reason) {
+        print('closed with $status for $reason');
+        
+        // close stream
+        stream.close();
+      };
     };
-  };
-  
-  server.listen('127.0.0.1', 8000);
+    
+    server.listen('127.0.0.1', 8000);
+  }
 }
