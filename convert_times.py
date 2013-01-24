5f989c407e7779325968a003e9386c99415e02cb
Christopher Best <bestchris@gmail.com>
2013-01-24 18:51:12 -0500

    wrote function to make datetime from time stamp string

--- convert_times.py
+++ convert_times.py
@@ -3,6 +3,14 @@ import re;
 from datetime import datetime;
 import time;
 
+def datetimefromstring(stamp_string):
+
+	# get a float of epoch time in seconds. have to divide by 1000 because dart timestamps are in ms
+	stamp = float(stamp_string) / 1000
+
+	# build a datetime object from the stamp
+	return datetime.fromtimestamp(stamp)
+
 # open file
 f = open('output/subject1/trial1/data.txt', 'r')
 
@@ -12,10 +20,4 @@ contents = f.read()
 # find trial start
 stamp_string = re.search('TrialStart (\d{13})', contents).group(1)
 
-# get a float of epoch time in seconds. have to divide by 1000 because dart timestamps are in ms
-stamp = float(stamp_string) / 1000
-
-# build a datetime object from the stamp
-start_date = datetime.fromtimestamp(float(stamp_string)/1000)
-
-print(start_date)
+print(datetimefromstring(stamp_string))
