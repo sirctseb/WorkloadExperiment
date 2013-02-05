498e5f165ca75ed5b3cc659d1f10932b874e942a
Christopher Best <bestchris@gmail.com>
2013-02-04 19:20:55 -0500

    added static access to optionally randomized list of all blocks

--- web/block.dart
+++ web/block.dart
@@ -18,5 +18,42 @@ class Block {
         targetNumberLow ? BlockTrialTask.LOW_TARGET_NUMBER : BlockTrialTask.HIGH_TARGET_NUMBER,
         additionDiffLow ? BlockTrialTask.LOW_OPERANDS : BlockTrialTask.HIGH_OPERANDS);
   }
+
+  static bool random = true;
+  static bool moreRandom = true;
+  static List<Block> _allBlocks;
+  /// A list of all experiment blocks
+  static List<Block> get allBlocks {
+    if(_allBlocks == null) {
+      _generateAllBlocks();
+    }
+    return _allBlocks;
+  }
+  static void _generateAllBlocks() {
+    _allBlocks = [];
+    var blocks = [
+      new Block(false, false, false),
+      new Block(false, false, true),
+      new Block(false, true, false),
+      new Block(false, true, true),
+      new Block(true, false, false),
+      new Block(true, false, true),
+      new Block(true, true, false),
+      new Block(true, true, true)
+    ];
+    if(random) {
+      Random rng;
+      if(moreRandom) {
+        rng = new Random(new Date.now().millisecond);
+      } else {
+        rng = new Random(0);
+      }
+      while(blocks.length > 0) {
+        _allBlocks.add(blocks.removeAt(rng.nextInt(blocks.length)));
+      }
+    } else {
+      _allBlocks = blocks;
+    }
+  }
   
 }
