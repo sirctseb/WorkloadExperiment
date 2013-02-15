cef490c6086faba33afcc363181db745981e3032
Christopher Best <bestchris@gmail.com>
2013-02-15 15:47:13 -0500

    fixed order of target means when computing concurrency also compute concurrency in loop

--- assembleData.R
+++ assembleData.R
@@ -52,6 +52,8 @@ assembleData <- function(subject) {
 	additionLowMean <<- mean(additionData[additionData$oprange==levels(additionData$oprange)[1], ]$complete)
 	# get the mean of the high addition only tasks
 	additionHighMean <<- mean(additionData[additionData$oprange==levels(additionData$oprange)[2], ]$complete)
+	# put in dataframe
+	additionMeans <<- data.frame(oprange = levels(additionData$oprange), mean = c(additionLowMean, additionHighMean))
 	
 	# get the mean of the low, low targeting only tasks
 	speedLow = levels(targetingData$speed)[1]
@@ -65,24 +67,43 @@ assembleData <- function(subject) {
 	targetHighLowMean <<- mean(targetingData[targetingData$speed == speedHigh & targetingData$difficulty == difficultyLow, ]$complete)
 	# get the mean of the low, low targeting only tasks
 	targetHighHighMean <<- mean(targetingData[targetingData$speed == speedHigh & targetingData$difficulty == difficultyHigh, ]$complete)
+	# put in dataframe
+	targetMeans <<- data.frame(speed = rep(levels(targetingData$speed), each=2), difficulty = rep(levels(targetingData$difficulty), 2),
+		mean = c(targetLowLowMean, targetLowHighMean, targetHighLowMean, targetHighHighMean))
 
 	# compute concurrency
 	mainData$concurrency[mainData$difficulty==difficultyLow & mainData$speed==speedLow & mainData$oprange==oprangeLow] <<-
 		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyLow & mainData$speed==speedLow & mainData$oprange==oprangeLow], additionLowMean, targetLowLowMean)
 	mainData$concurrency[mainData$difficulty==difficultyHigh & mainData$speed==speedLow & mainData$oprange==oprangeLow] <<-
-		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyHigh & mainData$speed==speedLow & mainData$oprange==oprangeLow], additionLowMean, targetHighLowMean)
+		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyHigh & mainData$speed==speedLow & mainData$oprange==oprangeLow], additionLowMean, targetLowHighMean)
 	mainData$concurrency[mainData$difficulty==difficultyLow & mainData$speed==speedHigh & mainData$oprange==oprangeLow] <<-
-		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyLow & mainData$speed==speedHigh & mainData$oprange==oprangeLow], additionLowMean, targetLowHighMean)
+		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyLow & mainData$speed==speedHigh & mainData$oprange==oprangeLow], additionLowMean, targetHighLowMean)
 	mainData$concurrency[mainData$difficulty==difficultyHigh & mainData$speed==speedHigh & mainData$oprange==oprangeLow] <<-
 		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyHigh & mainData$speed==speedHigh & mainData$oprange==oprangeLow], additionLowMean, targetHighHighMean)
 	mainData$concurrency[mainData$difficulty==difficultyLow & mainData$speed==speedLow & mainData$oprange==oprangeHigh] <<-
 		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyLow & mainData$speed==speedLow & mainData$oprange==oprangeHigh], additionHighMean, targetLowLowMean)
 	mainData$concurrency[mainData$difficulty==difficultyHigh & mainData$speed==speedLow & mainData$oprange==oprangeHigh] <<-
-		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyHigh & mainData$speed==speedLow & mainData$oprange==oprangeHigh], additionHighMean, targetHighLowMean)
+		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyHigh & mainData$speed==speedLow & mainData$oprange==oprangeHigh], additionHighMean, targetLowHighMean)
 	mainData$concurrency[mainData$difficulty==difficultyLow & mainData$speed==speedHigh & mainData$oprange==oprangeHigh] <<-
-		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyLow & mainData$speed==speedHigh & mainData$oprange==oprangeHigh], additionHighMean, targetLowHighMean)
+		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyLow & mainData$speed==speedHigh & mainData$oprange==oprangeHigh], additionHighMean, targetHighLowMean)
 	mainData$concurrency[mainData$difficulty==difficultyHigh & mainData$speed==speedHigh & mainData$oprange==oprangeHigh] <<-
 		getConcurrencyVec(mainData$complete[mainData$difficulty==difficultyHigh & mainData$speed==speedHigh & mainData$oprange==oprangeHigh], additionHighMean, targetHighHighMean)
+
+	# compute concurrency in a loop
+	for(o in unique(mainData$oprange)) {
+		for(d in unique(mainData$difficulty)) {
+			for(s in unique(mainData$speed)) {
+				mainData$concurrency2[mainData$oprange == o & mainData$difficulty == d & mainData$speed == s] <<-
+				getConcurrencyVec(mainData$complete[mainData$oprange == o & mainData$difficulty == d & mainData$speed == s],
+					additionMeans$mean[additionMeans$oprange == o],
+					targetMeans$mean[targetMeans$speed == s & targetMeans$difficulty == d])
+			}
+		}
+	}
+
+	# test that loop produces same as flat
+	print("concurrencies equal:")
+	print(all(mainData$concurrency == mainData$concurrency2))
 }
 
 getConcurrencyVec <- function(completionTimes, additionTime, targetTime) {
