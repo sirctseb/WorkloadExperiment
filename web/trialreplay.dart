1b0339a30d0965b0c81a9c50862400599841b58d
Christopher Best <bestchris@gmail.com>
2013-02-22 11:27:12 -0500

    defined time getter and backing field added iterationTime getter and setter

--- web/trialreplay.dart
+++ web/trialreplay.dart
@@ -3,8 +3,11 @@ part of WorkloadExperiment;
 /// A class to manage the replay of trials
 class TrialReplay {
   // trial state
-  /// The current time (in seconds from the start of the trial. can be negative)
-  num get time;
+  /// The current time in seconds from the start of the trial. can be negative
+  num get time => _time;
+  num _time;
+  /// The current time in seconds from the start of the iteration
+  num get iterationTime;
   /// The current iteration
   int iteration;
   /// The targets
@@ -19,8 +22,10 @@ class TrialReplay {
   // load data
   void loadTrial(String path);
   
-  /// Move the replay to a given time
+  /// Move the replay to a given trial time
   set time(num t);
+  /// Move the replay to a given iteration time
+  set iterationTime(num t);
   
   // ui elements
   InputElement iterationSlider;
