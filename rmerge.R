280842beb2c7e6bbceb75e0af488b277dda2cbfd
Christopher Best <bestchris@gmail.com>
2013-04-25 12:09:27 -0400

    combined r functions into one file

--- rmerge.R
+++
@@ -1,8 +0,0 @@
-rmerge <- function(df1, df2) {
-	cols <- intersect(colnames(df1) , colnames(df2))
-	rbind(df1[,cols, drop=FALSE], df2[,cols, drop=FALSE])
-}
-rmerge <- function(...) {
-	cols <- Reduce(intersect, llply(list(...), colnames))
-	Reduce(rbind, llply(list(...), function (df) {df[,cols, drop=FALSE]}))
-}
