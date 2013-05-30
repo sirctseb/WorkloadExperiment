b20a996fa47776594a313b93552100b90d7e4ad8
Christopher Best <bestchris@gmail.com>
2013-05-30 09:40:25 -0400

    use different transfer directory for ftp added workload processing stuff

--- putmodel.sh
+++ putmodel.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cp output/subject21/block$1/trial0/QN_ACTR_Model_Initialization.txt ~/Google\ Drive/Research/temp
+root=/Volumes/home
+# root=~/Google\ Drive
+# root=~/Desktop/unision-transfer
+cp output/subject$2/block$1/trial0/QN_ACTR_Model_Initialization.txt "${root}/Research/temp"
