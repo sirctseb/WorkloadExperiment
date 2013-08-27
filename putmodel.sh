aa069199cb8683e097c5a99ef47f79254f2bf9db
Christopher Best <bestchris@gmail.com>
2013-08-27 18:43:36 -0400

    changed root paths for transfers

--- putmodel.sh
+++ putmodel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-root=/Volumes/home
+root=/Volumes/cjbest
 # root=~/Google\ Drive
 # root=~/Desktop/unision-transfer
 cp output/subject$2/block$1/trial0/QN_ACTR_Model_Initialization.txt "${root}/Research/temp"
