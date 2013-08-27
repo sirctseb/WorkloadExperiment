aabbae37b7dd81a4e349ed11358a74af6b55f5eb
Christopher Best <bestchris@gmail.com>
2013-08-27 14:21:28 -0400

    changed root dir for transfers

--- putmodel.sh
+++ putmodel.sh
@@ -4,7 +4,7 @@
 
 # copies the model file for the given subject and block to Research/temp in the root directory below
 
-root=/Volumes/home
+root=/Volumes/cjbest
 # root=~/Google\ Drive
 # root=~/Desktop/unision-transfer
 cp output/subject$2/block$1/trial0/QN_ACTR_Model_Initialization.txt "${root}/Research/temp"
