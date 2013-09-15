9ded7ab64bdb8623f5ae2114263b87a735d95fca
Christopher Best <bestchris@gmail.com>
2013-09-15 17:54:28 -0400

    use terminal-notifier instead of growl

--- putAndRunAll.sh
+++ putAndRunAll.sh
@@ -3,7 +3,7 @@ BLOCKS=${3:-1 2 3 4 5 6 7 8 9 10 11 12 13 14}
 TRIALS=${4:-480}
 
 for block in $BLOCKS; do
-	growlnotify -m "putting block $block"
+	terminal-notifier -message "putting block $block"
 	echo putting block $block;
 	# write model file
 	./putmodel.sh $block $1;
@@ -12,7 +12,7 @@ for block in $BLOCKS; do
 done
 
 echo aggregating
-growlnotify -m "ran all blocks, aggregating"
+terminal-notifier -message "ran all blocks, aggregating"
 go run convert_times.go -s $1 -i $TRIALS
 
-growlnotify -m "totally done"
+terminal-notifier -message "totally done"
